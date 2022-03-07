cafde90271961bd42961a6bedd60c1beef40286e
haimhamiel <haim.hamiel@gmail.com>
2022-03-07 17:54:34 +0200

    convert to react Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,40 +1,34 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import './App.css';
 import ErrorBoundry from "../components/ErrorBoundry";
 
-class App extends Component{
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
 
-    componentDidMount(){
+function App() {
+
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then(Response => Response.json())
-            .then(users => this.setState({ robots: users}));   
-    }
+        .then(Response => Response.json())
+        .then(users => setRobots(users));
+      }, []);
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
     }
-
-    render(){
-        const { robots, searchfield } = this.state;
         const filteredRobots = robots.filter(robot =>{
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-            return !robots.length ? 
+        return !robots.length ? 
             <h1>Loading</h1> :
             (
                     <div className="tc">
                         <h1 className="f1">RoboFriends</h1>
-                        <SearchBox searchChange={this.onSearchChange}/>
+                        <SearchBox searchChange={onSearchChange}/>
                         <Scroll>
                             <ErrorBoundry>
                             <CardList robots={filteredRobots}/>
@@ -42,8 +36,5 @@ class App extends Component{
                         </Scroll>
                     </div>  
             );
-         
-    }
 }
-
 export default App;
